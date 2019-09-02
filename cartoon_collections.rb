b5e3c0dc08cf99fe1735a522ab8b61ee554faece
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-02 01:00:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(array)
 
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_array = array.select {|v| v =~ /[cheese_types]/ }
-array.find_all
+array.find_all { |i|  "#{i+1}" "#{cheese_types}" } 
 end
 # (1..10).find_all { |i|  i % 3 == 0 }   #=> [3, 6, 9]
 #
